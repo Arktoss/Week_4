12b0d44e9b4e06a8ee90da7207860f950b4980e4
Arktoss <adamB2106@gmail.com>
2022-09-12 13:02:12 -0400

    first commit

---
+++ bank_balance.py
@@ -0,0 +1,15 @@
+customer_name = input("Welcome, What is your name?")
+starting_balance = 5000.25
+
+print(f"Hello {customer_name} you're starting balance is {starting_balance}")
+
+pay_check = float(input("How much of your paycheck would you like to deposit?"))
+expenditure_item = input("Looks like you went shopping. What did you buy?")
+expenditure_cost = float(input(f"How much does the {expenditure_item} cost: "))
+
+def checking_balance(user_name, balance, deposits, expense_item, expense_amount): 
+
+    ending_balance = balance + deposits - expense_amount
+    print(f"Good day, {user_name}. After spending money on {expense_item} in the amount of {expense_amount}, your current checking balance is: {ending_balance}")
+
+checking_balance(customer_name, starting_balance, pay_check, expenditure_item, expenditure_cost)
